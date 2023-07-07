5dc750d54545035f7ba3b2d835f19cb6b48cfdc0
Martin142214 <mi_kalonkin@mail.bg>
2023-07-07 16:47:04 +0300

    create additional scripts for language track add set and change language method for lang dropdown define url reconstruction when select different language

--- src/main/resources/static/scripts/open_sideNavigation.js
+++ src/main/resources/static/scripts/open_sideNavigation.js
@@ -38,3 +38,75 @@ function closeNav() {
   footer.style.filter = "blur(0px)";
   footer.style.pointerEvents = "all";
 }
+
+function makeSecondaryNavResponsive(){
+  var x = document.getElementsByClassName("secondaryNav")[0];
+  if (x.className === "secondaryNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "secondaryNav";
+  }
+}
+
+function changeLanguage(){
+  var languageSelect = document.getElementById("language");
+  var lang = languageSelect.value;
+  var path = defineRedirectUrl(lang);
+  window.location.assign(path);
+}
+
+function defineRedirectUrl(language){
+  var uri = "";
+  var fullPath = "";
+  var protocolName = window.location.protocol;
+  var pageHostname = window.location.hostname;
+  var port = window.location.port;
+  var pagePathname = window.location.pathname;
+  const pathWords = pagePathname.split("/");
+  if (language == "english") {
+    if (pathWords.includes("bg") || pathWords.includes("fr")) {
+      // pattern: http: // localhost : 8080 /shoes
+      uri = protocolName.concat("//", pageHostname, ":", port);
+      fullPath = uri;
+      for (let i = 2; i < pathWords.length; i++) {
+        fullPath = fullPath.concat("/", pathWords[i]);      
+      }
+    }
+  }
+  
+  else if (language == "bulgarian") {
+    if (pathWords.includes("fr")) {
+      uri = protocolName.concat("//", pageHostname, ":", port, "/bg");
+      fullPath = uri;
+      for (let i = 2; i < pathWords.length; i++) {
+        fullPath = fullPath.concat("/", pathWords[i]);
+      }
+    }
+    else if(pathWords.includes("bg")){
+      fullPath = window.location.href;
+    }
+    else{
+      fullPath = protocolName.concat("//", pageHostname, ":", port, "/bg", pagePathname);
+    }
+  }
+
+  return fullPath;
+}
+
+function setLanguage(){
+  var lang = "";
+  var langDropdown = document.getElementById('language');
+  var pagePathname = window.location.pathname;
+  const pathWords = pagePathname.split("/");
+  if (pathWords.includes("bg")) {
+    lang = "bulgarian";
+  }
+  else{
+    lang = "english";
+  }
+  for (let i = 0; i < langDropdown.length; i++) {
+      if(langDropdown[i].value == lang){
+          langDropdown[i].setAttribute('selected', true);
+      }
+  }
+}
