f7f5b59b33b007f00023c6a2ccbb8053d969725a
Martin142214 <mi_kalonkin@mail.bg>
2023-07-07 17:26:17 +0300

    add privileges entity model

---
+++ src/main/java/com/example/demo/test/Models/entities/Privilege.java
@@ -0,0 +1,25 @@
+package com.example.demo.test.Models.entities;
+
+import java.util.Collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Roles_privileges")
+public class Privilege {
+ 
+    @Id
+    public String id;
+
+    public String name;
+
+    public Collection<Role> roles;
+
+    public Privilege() {
+
+    }
+    
+    public Privilege(String name) {
+        this.name = name;
+    }
+}
