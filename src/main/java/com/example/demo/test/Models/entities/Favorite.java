7802ee2347cc615485e142e58acdbbadbf24324e
Martin142214 <mi_kalonkin@mail.bg>
2023-07-07 17:29:53 +0300

    add favorites entity model

---
+++ src/main/java/com/example/demo/test/Models/entities/Favorite.java
@@ -0,0 +1,21 @@
+package com.example.demo.test.Models.entities;
+
+import java.util.Collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Favorites")
+public class Favorite {
+
+    @Id
+    public String id;
+
+    public Collection<Shoe> shoes;
+
+    public Collection<Clothes> clothes;
+
+    public User user;
+
+    public Favorite() {}
+}
