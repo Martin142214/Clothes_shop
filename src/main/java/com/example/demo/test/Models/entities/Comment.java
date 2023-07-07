343fd3961d021f53bd75f584f4ab4653dba6bc00
Martin142214 <mi_kalonkin@mail.bg>
2023-07-07 17:30:38 +0300

    add comments entity model

---
+++ src/main/java/com/example/demo/test/Models/entities/Comment.java
@@ -0,0 +1,31 @@
+package com.example.demo.test.Models.entities;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Comments")
+public class Comment {
+    @Id
+    public String id;
+
+    public String shoeId;
+
+    public String username;
+
+    public String content;
+
+    public Date date;
+
+    public Comment() {
+
+    }
+
+    public Comment(String shoeId, String username, String content, Date date) {
+        this.shoeId = shoeId;
+        this.username = username;
+        this.content = content;
+        this.date = date;
+    }
+}
