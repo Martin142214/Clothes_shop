8699f2eda27b05e152fe88bc4287976065f96cf7
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 18:08:10 +0300

    add clothes DTOs for sliders

---
+++ src/main/java/com/example/demo/test/Models/classModels/SliderClothes.java
@@ -0,0 +1,15 @@
+package com.example.demoTEST2.models.classModels;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.example.demoTEST2.models.entities.Clothes;
+
+public class SliderClothes {
+
+    public Collection<Clothes> clothesParts;
+
+    public SliderClothes() {
+        clothesParts = new ArrayList<>();
+    }
+}
