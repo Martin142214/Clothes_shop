d3fb14206432beffd683b7a97861f60f28ef5a73
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 19:52:59 +0300

    add repo for Role class

---
+++ src/main/java/com/example/demo/test/Repositories/authRepositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.test.Repositories.authRepositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.test.Models.entities.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String>{
+    
+    Role findByName(String name);
+}
