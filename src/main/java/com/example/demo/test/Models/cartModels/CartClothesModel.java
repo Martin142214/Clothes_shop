6d45d0378f7848e7902aefa415faa7df460bc454
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 18:25:56 +0300

    add cart model for clothes

---
+++ src/main/java/com/example/demo/test/Models/cartModels/CartClothesModel.java
@@ -0,0 +1,18 @@
+package com.example.demo.test.Models.cartModels;
+
+import com.example.demo.test.Models.enums.ClothesBrands;
+
+public class CartClothesModel {
+    public String id;
+    public ClothesBrands brand;
+    public String model;
+    public String releaseDate;
+    public String size;
+    public String colorSpecification;
+    public Integer price;
+    public String imagePath;
+
+    public CartClothesModel() {
+        
+    }
+}
