b20c26296aa4984083d69fbe60991307da2a44eb
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 19:54:27 +0300

    Revert "add repo for TurnOver class"
    This reverts commit 64e4c138bd32bd3a12ed2cd67657a015db947417.

--- src/main/java/com/example/demo/test/Repositories/TurnOverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.test.Repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.test.Models.entities.DailyTurnOver;
-
-public interface TurnOverRepository extends MongoRepository<DailyTurnOver, String>{
-    
-}
