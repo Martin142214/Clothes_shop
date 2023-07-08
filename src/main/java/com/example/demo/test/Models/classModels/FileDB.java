438d84748bbf4d6ea614931da9cbd96edc325cde
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 18:32:04 +0300

    add fileDB class contains necessary data for image files

---
+++ src/main/java/com/example/demo/test/Models/classModels/FileDB.java
@@ -0,0 +1,19 @@
+package com.example.demo.test.Models.classModels;
+
+public class FileDB {
+  
+    public String name;
+
+    public String type;
+
+    public String imagePath; 
+
+    public FileDB() {
+    }
+
+    public FileDB(String name, String type, String imagePath) {
+        this.name = name;
+        this.type = type;
+        this.imagePath = imagePath;
+    }
+}
