d12d6bb4ceb4a43b576297ee878131c641789056
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 18:33:06 +0300

    add Order class contains data for order - order number, name of the user and total price

---
+++ src/main/java/com/example/demo/test/Models/classModels/Order.java
@@ -0,0 +1,20 @@
+package com.example.demo.test.Models.classModels;
+
+public class Order {
+
+    public Integer orderNumber;
+
+    public String username;
+
+    public Integer priceSummaryFromOrder;
+
+    public Order() {
+        
+    }
+
+    public Order(Integer orderNumber, String username, Integer priceSummaryFromOrder) {
+        this.orderNumber = orderNumber;
+        this.username = username;
+        this.priceSummaryFromOrder = priceSummaryFromOrder;
+    }
+}
