57f639d59f2f551534cb851eb01691eabbec0297
Martin142214 <mi_kalonkin@mail.bg>
2023-07-08 18:08:25 +0300

    add shoes DTOs for sliders

---
+++ src/main/java/com/example/demo/test/Models/classModels/SliderShoes.java
@@ -0,0 +1,15 @@
+package com.example.demo.test.Models.classModels;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.example.demo.test.Models.entities.Shoe;
+
+public class SliderShoes {
+
+    public Collection<Shoe> shoesParts;
+
+    public SliderShoes() {
+        shoesParts = new ArrayList<>();
+    }
+}
