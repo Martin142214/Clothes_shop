e3017838d2407aff47a137eb968fdafa08a35562
Martin142214 <mi_kalonkin@mail.bg>
2023-07-10 13:12:18 +0300

    set paths for uploaded images in the "main" class

--- src/main/java/com/example/demo/test/DemoTestApplication.java
+++ src/main/java/com/example/demo/test/DemoTestApplication.java
@@ -2,11 +2,14 @@ package com.example.demo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoTestApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("images.path", "E:/Java IntelliJ projects/demo-test/src/main/resources/static/images/");
+		System.setProperty("clothes-images.path", "E:/Java IntelliJ projects/demo-test/src/main/resources/static/images/clothes-Images/");
 		SpringApplication.run(DemoTestApplication.class, args);
 	}
 
