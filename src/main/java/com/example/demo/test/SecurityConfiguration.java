59e07d93fe29d324d2d05cfd7964bd7d8db2ff28
Martin142214 <mi_kalonkin@mail.bg>
2023-07-11 12:57:04 +0300

    add security config path changes

--- src/main/java/com/example/demo/test/SecurityConfiguration.java
+++ src/main/java/com/example/demo/test/SecurityConfiguration.java
@@ -16,7 +16,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import com.example.demo.test.Services.UserDetailsServiceImplementation;
+
+import com.example.demo.test.services.UserDetailsServiceImplementation;
 
 @Configuration
 @EnableWebSecurity
